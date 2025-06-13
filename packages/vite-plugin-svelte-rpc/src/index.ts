07c236cbfcec1262f97c4ead048f9cce581642d7
hdzilyes <hdz.ilyes@gmail.com>
2025-06-13 11:15:41 +0200

    fix(virtual-module): use virtualModulesPlugin in the main plugin

--- packages/vite-plugin-svelte-rpc/src/index.ts
+++ packages/vite-plugin-svelte-rpc/src/index.ts
@@ -2,9 +2,12 @@ import type { Plugin } from 'vite';
 import type { PluginOptions } from './types';
 import { defaultOptions } from './core/options';
 import { RpcRegistry } from './core/rpc-registry';
+import { virtualModulesPlugin } from './virtual-modules';
 
 export default function vitePluginSvelteRpc(options: PluginOptions = defaultOptions): Plugin[] {
     const registry = new RpcRegistry();
 
-    return []
+    return [
+        virtualModulesPlugin()
+    ]
 }
